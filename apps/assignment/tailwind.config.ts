814d52fbd12471aeb96197efc98ed9986e633a8b
karkigrishmin <grishminkarki7@gmail.com>
2024-01-18 18:23:33 +0545

    feat: define box shadow in tailwind config and props to modal component

--- apps/assignment/tailwind.config.ts
+++ apps/assignment/tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
+const config: Pick<Config, "content" | "presets" | "theme"> = {
 	content: ["./src/**/*.tsx"],
 	presets: [sharedConfig],
+	theme: {
+		extend: {
+			boxShadow: {
+				modalBoxShadow: "100px 90px 50px #e06e9b",
+			},
+		},
+	},
 };
 
 export default config;
