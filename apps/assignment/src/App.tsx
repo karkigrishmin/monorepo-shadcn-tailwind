a492e96a96c058cab84702e9a4231275ec446ddb
karkigrishmin <grishminkarki7@gmail.com>
2024-01-18 20:24:46 +0545

    feat: integrate fake users api using react query and show users list in modal

--- apps/assignment/src/App.tsx
+++ apps/assignment/src/App.tsx
@@ -1,9 +1,85 @@
-import { Modal } from "@repo/assignment-ui/modal";
+import { DialogHeader, DialogTitle, Modal } from "@repo/assignment-ui/modal";
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+} from "../../../packages/assignment-ui/src/lib/avatar/avatar";
+
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { useGetUsers } from "./api/users/get-users";
 
 function App() {
+	const [usersLimit, setUsersLimit] = useState(5);
+
+	// const fetchUsers = async ({ queryKey }) => {
+	// 	console.log("query key", queryKey);
+	// 	const [_, limit] = queryKey;
+	// 	const response = await fetch(
+	// 		`https://random-data-api.com/api/v2/users?size=${limit}`
+	// 	);
+	// 	if (!response.ok) {
+	// 		throw new Error("Network response was not ok");
+	// 	}
+	// 	return response.json();
+	// };
+
+	// const userQuery = useQuery({
+	// 	queryKey: ["users", usersLimit],
+	// 	queryFn: fetchUsers,
+	// });
+
+	// const users: {
+	// 	avatar: string;
+	// 	date_of_birth: string;
+	// 	first_name: string;
+	// 	last_name: string;
+	// 	id: number;
+	// }[] = userQuery?.data ?? [];
+
+	const userQuery = useGetUsers({
+		params: { size: usersLimit },
+	});
+
+	const users = userQuery?.data;
+	console.log("user query", userQuery);
+	console.log("users", users);
+
 	return (
 		<>
-			<Modal className='!shadow-modalBoxShadow'></Modal>
+			<Modal className='!shadow-modalBoxShadow'>
+				<DialogHeader>
+					<DialogTitle>{users?.length} birthdays today</DialogTitle>
+				</DialogHeader>
+
+				<ul className='flex flex-col gap-5'>
+					{users?.map((eachUser) => {
+						const birthdate = new Date(eachUser.date_of_birth);
+
+						const currentDate = new Date();
+
+						const age = currentDate.getFullYear() - birthdate.getFullYear();
+
+						return (
+							<li key={eachUser.id} className='flex items-start gap-2'>
+								{/* <img src={eachUser.avatar} alt={eachUser.first_name} /> */}
+
+								<Avatar className='border-[1px] w-[3rem] h-[3rem]'>
+									<AvatarImage src={eachUser.avatar} />
+									<AvatarFallback>{eachUser.first_name}</AvatarFallback>
+								</Avatar>
+
+								<div className='flex flex-col'>
+									<h5>
+										{eachUser.first_name} {eachUser.last_name}
+									</h5>
+									<p className='text-gray-500'>{age} years</p>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</Modal>
 		</>
 	);
 }
--- apps/assignment/src/main.tsx
+++ apps/assignment/src/main.tsx
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.css";
 import "@repo/assignment-ui/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const fiveMinutesInMs = 5 * 60 * 1000;
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			refetchOnMount: true,
+			refetchOnReconnect: true,
+			retry: false,
+			staleTime: fiveMinutesInMs,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
