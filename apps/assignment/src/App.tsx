dcd53191dc407125447e25ae4b543e19999da930
karkigrishmin <grishminkarki7@gmail.com>
2024-01-18 13:04:29 +0545

    feat: add tailwindcss, tsup and write scripts to build and watch styles.css file in ui package

--- apps/assignment/src/App.tsx
+++ apps/assignment/src/App.tsx
@@ -1,7 +1,11 @@
+import Button from "@repo/assignment-ui/button";
+
 function App() {
 	return (
 		<>
 			<h1 className='text-center text-red-300'>test</h1>
+
+			<Button />
 		</>
 	);
 }
--- packages/assignment-ui/src/button.tsx
+++ packages/assignment-ui/src/button.tsx
@@ -1,20 +1,3 @@
-"use client";
-
-import { ReactNode } from "react";
-
-interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+export default function Button() {
+	return <button className='bg-slate-300 p-4'>button</button>;
 }
-
-export const Button = ({ children, className, appName }: ButtonProps) => {
-  return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
-      {children}
-    </button>
-  );
-};
